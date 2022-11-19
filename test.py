5786dd2ed2180a98f3c250f63b451c9573aae049
MidhatHasan <midhathasan@outlook.com>
2022-11-19 23:22:54 +0000

    big dub

--- test.py
+++ test.py
@@ -1,4 +1,19 @@
-print("hello world")
+import csv
 
+header = ['Time', 'Price']
+data = [
+    [0.0, 2874800.0],
+    [1.0, 23817410.0],
+    [2, 199000.0],
+    [3, 468000.0],
+    [4, 1246700.0]
+]
 
-print("hello")
+with open('scatter.csv', 'w', encoding='UTF8', newline='') as f:
+    writer = csv.writer(f)
+
+    # write the header
+    writer.writerow(header)
+
+    # write multiple rows
+    writer.writerows(data)
